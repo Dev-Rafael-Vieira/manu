4661095716af0e920178777094d591f40d9daa9d
Dev-Rafael-Vieira <rafael_egqsociesc@hotmail.com>
2022-01-30 22:37:13 -0300

    inicial

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+
+import Image from 'next/image'
+export default function Home() {
+  return (
+    <div className="manu">
+    <h1>Eu Te</h1>
+     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/220px-Heart_coraz%C3%B3n.svg.png" />
+    <h1>Manu</h1>
+   </div>
+  )
+}
